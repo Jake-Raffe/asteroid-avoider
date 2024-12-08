ca5c6407b1fc80aa7d79403922708869fa7bad9f
Jacob Raffe <jakeraffe24@gmail.com>
2024-12-08 20:16:45 +0000

    Fix window/scene dimension mismatch and collision bugs

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 ### Set Up
 
-The app requires standard sbt set up. Compile with `sbt compile`, run with `sbt run`.
+The app requires a standard sbt set up. Use `sbt compile` and `sbt run` to run.
 Running the application will start the game in a new window.
 
 ### How to Play
-
-...
+- Press `SPACE` to start or pause the game.
+- Move the spaceship with the `LEFT` and `RIGHT` arrow keys to avoid the asteroids.
+- Collision with an asteroid will invert the colours and end the game.
+- Press `R` to reset the game.
+- Game speed will increase over time.
--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,8 +1,9 @@
 import ConfigConstants.*
-import javafx.scene.input.KeyCode
+import javafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.beans.property.*
 import scalafx.scene.Scene
+import scalafx.scene.layout.StackPane
 import scalafx.scene.paint.Color
 import scalafx.scene.paint.Color.*
 import scalafx.scene.shape.{Rectangle, Sphere}
@@ -14,7 +15,7 @@ import scala.util.Random
 object Game extends JFXApp3 {
 
   override def start(): Unit = {
-    val state: ObjectProperty[State]      = ObjectProperty(initialStateWithAsteroids)
+    val state: ObjectProperty[State]      = ObjectProperty(initialState)
     val frame: IntegerProperty            = IntegerProperty(0)
     val frameIncrementor: IntegerProperty = IntegerProperty(0)
     val scrollAccelerator: LongProperty   = LongProperty(0)
@@ -28,40 +29,35 @@ object Game extends JFXApp3 {
     }
 
     stage = new JFXApp3.PrimaryStage {
+      title = "Asteroid Avoider"
       width = sceneXBoundary
       height = sceneYBoundary
       scene = new Scene {
         fill = Black
         content = state.value.generateAllObjects
-        onKeyPressed = key =>
-          key.getCode match {
-            case KeyCode.SPACE if state.value.gameInMotion =>
-              println(">>> Game PAUSED <<<")
-              frameIncrementor.update(0)
-              state.update(state.value.pauseGame())
-            case KeyCode.SPACE =>
-              println("<<< Game STARTED >>>")
-              frameIncrementor.update(1)
-              state.update(state.value.startGame())
-            case KeyCode.LEFT =>
-              println("<<< Left <<<")
-              state.update(state.value.moveShip(Left))
-            case KeyCode.RIGHT =>
-              println(">>> Right >>>")
-              state.update(state.value.moveShip(Right))
-            case KeyCode.R =>
-              println("*** Game RESET ***")
-              resetGame()
-            case other => // Log for reference
-              println(s"*** other key pressed: '$other'")
-          }
-
+        onKeyPressed = handleKeyPress(_)
         // 4. when state is updated, regenerate game field
-        state.onChange(Platform.runLater {
-          content = state.value.generateAllObjects
-        })
+        state.onChange {
+          if (state.value.collision) frameIncrementor.update(0)
+          Platform.runLater {
+            content = state.value.generateAllObjects
+          }
+        }
       }
     }
+
+    // Scene: this is the game field that the game actually exists in
+    // Stage/Window: this is the window that we see, W/H can be set and edited
+    // Scene is always slightly larger than the stage on creation for some reason. Has to be edited in post:
+    stage.setOnShown { _ =>
+      stage.width = stage.scene().getWidth + objectWidth
+      stage.height = stage.scene().getHeight + (3 * objectWidth)
+    }
+
+    // 1. start game loop
+    // GameLoop can only ever be called once or multiple threads will run
+    // To pause / play the game, the incrementer must be edited to that the thread is continuous but the frame is not always changing
+    gameLoop()
 
     // 2. game loop thread will increment frame every 1s, triggering the .onChange method
     def gameLoop(): Unit =
@@ -74,13 +70,30 @@ object Game extends JFXApp3 {
       scrollAccelerator.update(0)
       frameIncrementor.update(0)
       frame.update(0)
-      state.update(initialStateWithAsteroids)
+      state.update(initialState)
     }
 
-    // 1. start game loop
-    // GameLoop can only ever be called once or multiple threads will run
-    // To pause / play the game, the incrementer must be edited to that the thread is continuous but the frame is not always changing
-    gameLoop()
+    def handleKeyPress(key: KeyEvent): Unit = key.getCode match {
+      case KeyCode.SPACE if state.value.gameInMotion =>
+        println(">>> Game PAUSED <<<")
+        frameIncrementor.update(0)
+        state.update(state.value.pauseGame())
+      case KeyCode.SPACE =>
+        println("<<< Game STARTED >>>")
+        frameIncrementor.update(1)
+        state.update(state.value.startGame())
+      case KeyCode.LEFT =>
+        println("<<< Left <<<")
+        state.update(state.value.moveShip(Left))
+      case KeyCode.RIGHT =>
+        println(">>> Right >>>")
+        state.update(state.value.moveShip(Right))
+      case KeyCode.R =>
+        println("*** Game RESET ***")
+        resetGame()
+      case other => // Log for reference
+        println(s"*** other key pressed: '$other'")
+    }
   }
 
 }
